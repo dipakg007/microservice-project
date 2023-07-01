d045877a21f0e176164b6ad4a69ba607812cfa16
Dipak Gupta <dipakg286@gmail.com>
2023-07-01 14:45:45 +0530

    Service registry added

--- employee-service/src/main/java/com/project/employee/service/DepartmentServiceProxy.java
+++ employee-service/src/main/java/com/project/employee/service/DepartmentServiceProxy.java
@@ -2,12 +2,11 @@ package com.project.employee.service;
 
 import com.project.domain.dto.DepartmentDto;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @FeignClient(url = "http://localhost:8080", value = "DEPARTMENT-SERVICE-PROXY")
 public interface DepartmentServiceProxy {
     @GetMapping("/api/departments/departmentCode/{departmentCode}")
-    public DepartmentDto getDepartmentByCode(@PathVariable String departmentCode);
+    DepartmentDto getDepartmentByCode(@PathVariable String departmentCode);
 }
